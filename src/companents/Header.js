f4d569c1a07238915202ab119bd1673cbd992378
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-30 02:09:55 +0300

    Add new font awesome icons and babel-plugin-macros

--- src/companents/Header.js
+++ src/companents/Header.js
@@ -1,15 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 function Header() {
   return (
     <div>
       <div class="  flex bg-colors-lacivert  justify-around flex-row">
         <div class=" flex text-colors-white ">
           <h6>(225) 555-0118 </h6>
-          <h6>michelle.rivera@example.com</h6>
+
           <div class=" flex">
             <h6>Follow Us and get a chance to win 80% off</h6>
           </div>
           <div class=" flex">
             <h6>Follow Us:</h6>
+            <FontAwesomeIcon icon />
+            <FontAwesomeIcon icon={faCoffee} />
           </div>
         </div>
       </div>
