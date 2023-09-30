eb4b5d2bb6ea1121e7eb7671cd3d51da68fc99f8
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-30 16:43:37 +0300

    Update social media icons and contact info in Header component

--- src/companents/Header.js
+++ src/companents/Header.js
@@ -1,21 +1,36 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { BsTelephone } from "react-icons/bs";
+import { TfiEmail } from "react-icons/tfi";
+import { BsInstagram } from "react-icons/bs";
+import { FaYoutube } from "react-icons/fa";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+
 function Header() {
   return (
     <div>
-      <div class="  flex bg-colors-lacivert  justify-around flex-row">
-        <div class=" flex text-colors-white ">
-          <h6>(225) 555-0118 </h6>
+      <div class="  flex bg-colors-lacivert  text-colors-white place-content-around p-4">
+        <div class=" flex ">
+          <div class="flex  items-center">
+            <BsTelephone />
+            <h6 class="pl-1">(225) 555-0118 </h6>
+          </div>
 
-          <div class=" flex">
-            <h6>Follow Us and get a chance to win 80% off</h6>
+          <div class="flex items-center pl-4">
+            <TfiEmail />
+            <h6 class="pl-1">michelle.rivera@example.com</h6>
           </div>
-          <div class=" flex">
-            <h6>Follow Us:</h6>
-            <FontAwesomeIcon icon />
-            <FontAwesomeIcon icon={faCoffee} />
-          </div>
+        </div>
+
+        <div class=" flex items-center">
+          <h6>Follow Us and get a chance to win 80% off</h6>
+        </div>
+
+        <div class=" flex items-center space-x-2">
+          <h6 class="pr-2">Follow Us:</h6>
+          <BsInstagram />
+          <FaYoutube />
+          <BsFacebook />
+          <BsTwitter />
         </div>
       </div>
       <div>
