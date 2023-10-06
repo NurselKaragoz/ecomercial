34ae9f5988d57b85f23f13495b49b1f2b43d93ca
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-06 13:17:40 +0300

    about page tamamlandı

--- src/companents/AboutPage.js
+++ src/companents/AboutPage.js
@@ -1,4 +1,5 @@
 import AboutCompany from "./AboutCompany";
+import AboutMeet from "./AboutMeet";
 import AboutNumbers from "./AboutNumbers";
 import AboutProblem from "./AboutProblem";
 import AboutVideoCard from "./AboutVideoCard";
@@ -13,6 +14,7 @@ function AboutPage() {
       <AboutProblem />
       <AboutNumbers />
       <AboutVideoCard />
+      <AboutMeet />
       <ClientsAdd />
       <Clients />
       <AboutWork />
