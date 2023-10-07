8327c74676ce9f9f2d55b18ef964295fbfc5cb2e
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-07 11:51:50 +0300

    Add TeamCard component and update Navigator links

--- src/companents/AboutMeet.js
+++ src/companents/AboutMeet.js
@@ -1,7 +1,5 @@
-import teampic from "./images/team-user.jpg";
-import { BsInstagram } from "react-icons/bs";
-import { BsFacebook } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs";
+import TeamCard from "./TeamCard";
+
 function AboutMeet() {
   return (
     <div className="p-20">
@@ -10,27 +8,10 @@ function AboutMeet() {
         Problems trying to resolve the conflict between <br />
         the two major realms of Classical physics: Newtonian mechanics
       </p>
-      <div className=" flex justify-center pt-10">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src={teampic} />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2 text-colors-lacivert">
-              Username
-            </div>
-            <p class="text-gray-700 text-base text-colors-gray">Proffesion</p>
-          </div>
-          <div class="px-6 pt-4 pb-2 text-colors-blue">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              <BsInstagram />
-            </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              <BsFacebook />
-            </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              <BsTwitter />
-            </span>
-          </div>
-        </div>
+      <div className=" flex flex-row justify-center gap-5">
+        <TeamCard />
+        <TeamCard />
+        <TeamCard />
       </div>
     </div>
   );
