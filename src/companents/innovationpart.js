6b8f5aa9cb0a6cc6eedf59bc8c62ae7f4f77d625
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-23 22:37:07 +0300

    Refactor styling in InnovationPart component

--- src/companents/innovationpart.js
+++ src/companents/innovationpart.js
@@ -2,21 +2,18 @@ import innovationpic from "./images/innovationpic.png";
 import innovationpic2 from "./images/innovationpic2.png";
 import innovationpic3 from "./images/innovation3.png";
 import innovationpic4 from "./images/innovationpic4.png";
-
 import innovationpic5 from "./images/innovationpic5.png";
 
 function InnovationPart() {
   return (
     <div>
-      <div className=" justify-center pb-10">
-        <h5 className=" text-colors-gray">WHAT WE DO</h5>
-        <h2 className=" text-colors-lacivert">Innovation tailored for you</h2>
+      <div className="justify-center pb-10">
+        <h5 className="text-colors-gray">WHAT WE DO</h5>
+        <h2 className="text-colors-lacivert">Innovation tailored for you</h2>
       </div>
-      <div className=" flex flex-row ">
-        <div className=" flex flex-start object-cover">
-          <img src={innovationpic} />
-        </div>
-        <div className=" flex gap-2 flex-wrap flex-row justify-end">
+      <div className="flex flex-col md:flex-row">
+        <img src={innovationpic} className="w-full md:w-1/1 object-cover" />
+        <div className="flex gap-2 flex-wrap flex-row ">
           <img src={innovationpic2} />
           <img src={innovationpic3} />
           <img src={innovationpic4} />
@@ -26,4 +23,5 @@ function InnovationPart() {
     </div>
   );
 }
+
 export default InnovationPart;
