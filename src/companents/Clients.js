8a7b05982d0f441a7c0290a26601986fbdc693e6
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-23 20:24:29 +0300

    Add React import to Clients, Header, and Navigator

--- src/companents/Clients.js
+++ src/companents/Clients.js
@@ -1,3 +1,4 @@
+import React from "react";
 import img1 from "./images/col-md-2.svg";
 import img2 from "./images/col-md-2-1.svg";
 import img3 from "./images/col-md-2-2.svg";
@@ -7,16 +8,41 @@ import img6 from "./images/col-md-2-5.svg";
 
 function Clients() {
   return (
-    <div>
-      <div className=" flex flex-row justify-around mt-3 items-center pb-10 pt-10">
-        <img src={img1}></img>
-        <img src={img6}></img>
-        <img src={img2}></img>
-        <img src={img3}></img>
-        <img src={img4}></img>
-        <img src={img5}></img>
+    <div className="py-10">
+      <div className="flex flex-col md:flex-row justify-around items-center">
+        <img
+          src={img1}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 1"
+        />
+        <img
+          src={img6}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 2"
+        />
+        <img
+          src={img2}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 3"
+        />
+        <img
+          src={img3}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 4"
+        />
+        <img
+          src={img4}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 5"
+        />
+        <img
+          src={img5}
+          className="max-w-xs md:max-w-md mb-4 md:mb-0"
+          alt="Client 6"
+        />
       </div>
     </div>
   );
 }
+
 export default Clients;
--- src/companents/Navigator.js
+++ src/companents/Navigator.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { IoPersonOutline } from "react-icons/io5";
 import { BiSearch } from "react-icons/bi";
 import { LiaShoppingCartSolid } from "react-icons/lia";
@@ -7,38 +8,44 @@ import { Link } from "react-router-dom";
 
 function Navigator() {
   return (
-    <div className="flex items-center p-4  justify-between ">
-      <div>
-        <h3 className=" text-colors-lacivert font-semibold  text-xl">
-          Bandage
-        </h3>
-      </div>
-      <div className="text-colors-gray list-none flex flex-row space-x-3 items-center ">
-        <Link className=" text-colors-gray no-underline" to="/">
-          <li>Home</li>
-        </Link>
-        <Link className=" text-colors-gray no-underline" to="/products">
-          <li>Shop</li>
-        </Link>
-        <RiArrowDropDownLine />
-        <Link className=" text-colors-gray no-underline" to="/about">
-          <li>About</li>
-        </Link>
-        <Link className=" text-colors-gray no-underline" to="/contact">
-          <li>Contact</li>
-        </Link>
-        <Link className=" text-colors-gray no-underline" to="/team">
-          <li>Team</li>
-        </Link>
-      </div>
-      <div className="text-colors-blue flex items-center space-x-2 ">
-        <IoPersonOutline />
-        Login / Register
-        <BiSearch />
-        <LiaShoppingCartSolid />1
-        <AiOutlineHeart />1
+    <div className=" text-colors-white p-4">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div className="mb-4 md:mb-0">
+          <h3 className="text-xl font-semibold text-colors-lacivert ">
+            Bandage
+          </h3>
+        </div>
+        <div className="text-colors-gray list-none flex flex-row space-x-3 items-center mb-4 flex-wrap md:mb-0 md:flex md:flex-col">
+          <Link
+            className="text-colors-gray no-underline flex md:flex-col"
+            to="/"
+          >
+            <li>Home</li>
+          </Link>
+          <Link className="text-colors-gray no-underline" to="/products">
+            <li>Shop</li>
+          </Link>
+          <RiArrowDropDownLine />
+          <Link className="text-colors-gray no-underline" to="/about">
+            <li>About</li>
+          </Link>
+          <Link className="text-colors-gray no-underline" to="/contact">
+            <li>Contact</li>
+          </Link>
+          <Link className="text-colors-gray no-underline" to="/team">
+            <li>Team</li>
+          </Link>
+        </div>
+        <div className="text-colors-blue flex items-center space-x-2">
+          <IoPersonOutline />
+          <span className="hidden md:inline">Login / Register</span>
+          <BiSearch />
+          <LiaShoppingCartSolid />1
+          <AiOutlineHeart />1
+        </div>
       </div>
     </div>
   );
 }
+
 export default Navigator;
