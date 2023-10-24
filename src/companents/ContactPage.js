25e1bf15ae3167674ecf19dd51d8339c4eeb1f45
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-24 13:35:04 +0300

    contact page responsive yapıldı

--- src/companents/ContactPage.js
+++ src/companents/ContactPage.js
@@ -1,34 +1,34 @@
 import imagebg from "./images/about-bg.png";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaYoutube } from "react-icons/fa";
-import { BsFacebook } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs";
+import { BsFacebook, BsTwitter } from "react-icons/bs";
 import AboutContact from "./AboutContact";
 import LetsTalk from "./LetsTalk";
+
 function ContactPage() {
   return (
-    <div>
-      <div className=" flex flex-row justify-center">
-        <div className=" text-colors-lacivert text-left">
-          <h5 className=" pb-5">CONTACT US</h5>
-          <h1 className=" pb-3">
+    <div className="p-4 md:p-10">
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between">
+        <div className="text-colors-lacivert text-left md:w-1/2 md:pr-6">
+          <h5 className="pb-3 md:pb-5 text-2xl">CONTACT US</h5>
+          <h1 className="pb-3 text-4xl md:text-5xl">
             Get in touch <br /> today!
           </h1>
-          <h4 className=" text-colors-gray">
+          <h4 className="text-colors-gray pb-3 md:pb-4">
             We know how large objects will act, <br />
             but things on a small scale
           </h4>
-          <h3>Fax : +451 215 215</h3>
-          <h3>Phone ; +451 215 215 </h3>
-          <div className=" flex flex-row gap-3 pt-5">
+          <h3>Fax: +451 215 215</h3>
+          <h3>Phone: +451 215 215</h3>
+          <div className="flex flex-row gap-3 pt-3 md:pt-5 text-2xl">
             <AiFillInstagram />
             <FaYoutube />
             <BsFacebook />
             <BsTwitter />
           </div>
         </div>
-        <div>
-          <img src={imagebg} />
+        <div className="md:w-1/2 pt-5 md:pt-0">
+          <img src={imagebg} alt="Contact Us Background" />
         </div>
       </div>
       <AboutContact />
@@ -36,4 +36,5 @@ function ContactPage() {
     </div>
   );
 }
+
 export default ContactPage;
