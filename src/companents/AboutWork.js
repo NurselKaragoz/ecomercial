dab943a0f20668af61296438db1285caff196828
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-24 10:56:41 +0300

    Update styles for AboutCompany, AboutMeet, AboutNumbers, AboutProblem, AboutWork, and TeamCard components

--- src/companents/AboutWork.js
+++ src/companents/AboutWork.js
@@ -1,22 +1,24 @@
 import workimg from "./images/workwithus.png";
+
 function AboutWork() {
   return (
-    <div className=" flex flex-row bg-colors-blue200 justify-between">
-      <div className=" text-colors-white text-left  flex flex-col justify-center pl-36">
+    <div className="flex flex-col md:flex-row justify-between bg-colors-blue200">
+      <div className="text-colors-white text-left flex flex-col justify-center md:w-1/2 p-6 md:pl-36">
         <h5>Work With Us</h5>
         <h2>Now Let’s grow Yours</h2>
         <p>
           The gradual accumulation of information about atomic and <br />
           small-scale behavior during the first quarter of the 20th
         </p>
-        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border  border-colors-white hover:border-transparent rounded">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border w-fit border-colors-white hover:border-transparent rounded">
           Button
         </button>
       </div>
-      <div className=" flex justify-end">
-        <img src={workimg} />
+      <div className="md:w-1/2 flex justify-center">
+        <img src={workimg} alt="Work With Us" className="object-cover w-full" />
       </div>
     </div>
   );
 }
+
 export default AboutWork;
