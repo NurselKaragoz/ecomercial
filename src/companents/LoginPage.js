d8b47b4230b4a0fad2a7a17544a2b685b25b5359
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-10-30 23:35:44 +0300

    store kullanıcısına bağlı inputlar eklendi

--- src/companents/LoginPage.js
+++ src/companents/LoginPage.js
@@ -34,7 +34,6 @@ export default function App() {
 
   const onSubmit = (data) => {
     console.log("form submit", data);
-    console.log("role id =>", data.role_id);
   };
 
   // axios
@@ -45,90 +44,122 @@ export default function App() {
 
   return (
     <div className="gap-5 flex flex-col">
-      <div>
-        <label>Role</label>
-        <br />
-        <select
-          className=" border-2 border-colors-lacivert rounded-md py-1 px-5"
-          value={registerMode}
-          onChange={(e) => setRegisterMode(e.target.value)}
-        >
-          <option value="1">customer</option>
-          <option value="2">store</option>
-          <option value="3">admin</option>
-        </select>
-      </div>
-      {registerMode === "1" && (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className=" flex- flex-col gap-3">
-            <label>Name</label>
-            <div>
-              <br />
-              <input
-                className="border-2 border-colors-lacivert rounded-md py-1 px-5"
-                type="text"
-                name="name"
-                {...register("name")}
-              />
-
-              <p className="text-colors-red"> {errors.name?.message}</p>
-            </div>
-          </div>
-          <div className=" gap-3">
-            <label>Email</label>
-            <br />
-            <input
-              className=" border-2 border-colors-lacivert rounded-md py-1 px-5 gap-3"
-              type="text"
-              name="email"
-              {...register("email")}
-            />
-
-            <p className="text-colors-red"> {errors.email?.message}</p>
-          </div>
-          <div className="">
-            <label>Password</label>
-            <br />
-            <input
-              name="password"
-              type="password"
-              {...register("password")}
-              className={` ${
-                errors.password ? "is-invalid" : ""
-              } border-2 border-colors-lacivert rounded-md py-1 px-5 `}
-            />
-
-            <p className="text-colors-red"> {errors.password?.message}</p>
-          </div>
-          <div></div>
-
-          <div className="">
-            <label>Confirm Password</label>
-            <br />
-            <input
-              name="confirmPassword"
-              type="password"
-              {...register("confirmPassword")}
-              className={` ${
-                errors.confirmPassword ? "is-invalid" : ""
-              } border-2 py-1 px-5 rounded-md`}
-            />
-            <div className="invalid-feedback ">
-              {errors.confirmPassword?.message}
-            </div>
-          </div>
-
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Role</label>
+          <br />
+          <select
+            className=" border-2 border-colors-lacivert rounded-md py-1 px-5"
+            value={registerMode}
+            {...register("role_id")}
+            onChange={(e) => setRegisterMode(e.target.value)}
+          >
+            <option value="1">customer</option>
+            <option value="2">store</option>
+            <option value="3">admin</option>
+          </select>
+        </div>
+        <div className=" flex- flex-col gap-3">
+          <label>Name</label>
           <div>
-            <div>{` $data.role_id`} fghjkl</div>
-            <button
-              className=" bg-colors-lacivert px-5 py-2 text-colors-white rounded-md mt-4 mb-4"
-              type="submit"
-            >
-              Login
-            </button>
+            <br />
+            <input
+              className="border-2 border-colors-lacivert rounded-md py-1 px-5"
+              type="text"
+              name="name"
+              {...register("name")}
+            />
+
+            <p className="text-colors-red"> {errors.name?.message}</p>
           </div>
-        </form>
-      )}
+        </div>
+        <div className=" gap-3">
+          <label>Email</label>
+          <br />
+          <input
+            className=" border-2 border-colors-lacivert rounded-md py-1 px-5 gap-3"
+            type="text"
+            name="email"
+            {...register("email")}
+          />
+
+          <p className="text-colors-red"> {errors.email?.message}</p>
+        </div>
+        <div className="">
+          <label>Password</label>
+          <br />
+          <input
+            name="password"
+            type="password"
+            {...register("password")}
+            className={` ${
+              errors.password ? "is-invalid" : ""
+            } border-2 border-colors-lacivert rounded-md py-1 px-5 `}
+          />
+
+          <p className="text-colors-red"> {errors.password?.message}</p>
+        </div>
+        <div></div>
+
+        <div className="">
+          <label>Confirm Password</label>
+          <br />
+          <input
+            name="confirmPassword"
+            type="password"
+            {...register("confirmPassword")}
+            className={` ${
+              errors.confirmPassword ? "is-invalid" : ""
+            } border-2 py-1 px-5 rounded-md`}
+          />
+          <div className="invalid-feedback ">
+            {errors.confirmPassword?.message}
+          </div>
+        </div>
+        <div>
+          {registerMode === "2" && (
+            <div>
+              <div className=" gap-3">
+                <label>Store Name</label>
+                <br />
+                <input
+                  className=" border-2 border-colors-lacivert rounded-md py-1 px-5 gap-3"
+                  type="text"
+                  name="store"
+                  {...register("store")}
+                />
+              </div>
+              <div className=" gap-3">
+                <label>Store Tax ID </label>
+                <br />
+                <input
+                  className=" border-2 border-colors-lacivert rounded-md py-1 px-5 gap-3"
+                  type="number"
+                  name="tax_no"
+                />
+              </div>
+              <div className=" gap-3">
+                <label>Store Bank Account</label>
+                <br />
+                <input
+                  className=" border-2 border-colors-lacivert rounded-md py-1 px-5 gap-3"
+                  type="number"
+                  name="bank_account"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <button
+            className=" bg-colors-lacivert px-5 py-2 text-colors-white rounded-md mt-4 mb-4"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
