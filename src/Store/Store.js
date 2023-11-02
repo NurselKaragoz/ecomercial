d798afed73ae6ceb85312c16df382a99feba9a52
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-02 22:44:39 +0300

    Add Redux store configuration and global actions

---
+++ src/Store/Store.js
@@ -0,0 +1,25 @@
+import { legacy_createStore as createStore } from "redux";
+
+import { applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import globalReducer from "./reducers/globalReducer";
+import userReducer from "./reducers/userReducer";
+import productReducer from "./reducers/productReducer";
+import shoppingCartReducer from "./reducers/shoppingCartReducer";
+import storeReducer from "./reducers/storeReducer";
+
+const reducer = combineReducers({
+  global: globalReducer,
+  user: userReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer,
+  store: storeReducer,
+});
+
+const middleware = [thunk, logger];
+
+const store = createStore(reducer);
+
+export default store;
---
+++ src/Store/reducers/storeReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  storeInfo: null,
+};
+
+const storeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default storeReducer;
