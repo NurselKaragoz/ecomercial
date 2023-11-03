363419e6cac711424961c33bdb52260badea997c
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-03 13:24:22 +0300

    Add new actions and reducers

--- src/Store/reducers/shoppingCartReducer.js
+++ src/Store/reducers/shoppingCartReducer.js
@@ -1,3 +1,9 @@
+import {
+  SET_ADDRESS,
+  SET_CART,
+  SET_PAYMENT,
+} from "../Actions/shoppingCartAction";
+
 const initialState = {
   cart: [],
   payment: {},
@@ -6,8 +12,21 @@ const initialState = {
 
 const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
-    default:
-      return state;
+    case SET_CART:
+      return {
+        ...state,
+        theme: action.payload,
+      };
+    case SET_PAYMENT:
+      return {
+        ...state,
+        theme: action.payload,
+      };
+    case SET_ADDRESS:
+      return {
+        ...state,
+        theme: action.payload,
+      };
   }
 };
 
--- src/Store/reducers/storeReducer.js
+++ src/Store/reducers/storeReducer.js
@@ -1,11 +1,34 @@
+import {
+  SET_STOREIBANNO,
+  SET_STORENAME,
+  SET_STORETAXNO,
+} from "../Actions/storeActions";
+
 const initialState = {
-  storeInfo: null,
+  storeName: "",
+  storeTaxNo: "",
+  storeIbanNo: "",
 };
 
 const storeReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_STORENAME:
+      return {
+        ...state,
+        theme: action.payload,
+      };
+
+    case SET_STORETAXNO:
+      return {
+        ...state,
+        theme: action.payload,
+      };
+    case SET_STOREIBANNO:
+      return {
+        ...state,
+        theme: action.payload,
+      };
     default:
-      return state;
   }
 };
 
