be6ca7cb41890e45bd7feb5137915fbb5f5e2c62
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-04 11:48:41 +0300

    completed shoppingcartreducer and actions

--- src/Store/reducers/shoppingCartReducer.js
+++ src/Store/reducers/shoppingCartReducer.js
@@ -1,3 +1,10 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  SET_PAYMENT,
+  SET_ADDRESS,
+} from "../Actions/shoppingCartActions";
 const initialState = {
   cart: [],
   payment: {},
@@ -6,6 +13,31 @@ const initialState = {
 
 const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, { count: action.count, product: action.product }],
+      };
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.product.id !== action.productId),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cart: [],
+      };
+    case SET_PAYMENT:
+      return {
+        ...state,
+        payment: action.paymentInfo,
+      };
+    case SET_ADDRESS:
+      return {
+        ...state,
+        address: action.addressInfo,
+      };
     default:
       return state;
   }
