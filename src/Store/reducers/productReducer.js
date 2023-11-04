d9b3bd698f678bef25116bf97442df137e6f895b
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-04 11:38:23 +0300

    Add product actions and reducer for managing product state

--- src/Store/reducers/productReducer.js
+++ src/Store/reducers/productReducer.js
@@ -1,12 +1,45 @@
+import {
+  SET_PRODUCT_LIST,
+  SET_TOTAL_PRODUCT_COUNT,
+  SET_PAGE_COUNT,
+  SET_ACTIVE_PAGE,
+  SET_FETCH_STATE,
+} from "../Actions/productActions";
 const initialState = {
   productList: [],
-  totalProductCount: [],
-  pageCount: [],
-  activePage: [],
-  fetchState: [],
+  totalProductCount: 0,
+  pageCount: 0,
+  activePage: 1,
+  fetchState: "NOT_FETCHED",
 };
+
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_PRODUCT_LIST:
+      return {
+        ...state,
+        productList: action.productList,
+      };
+    case SET_TOTAL_PRODUCT_COUNT:
+      return {
+        ...state,
+        totalProductCount: action.totalProductCount,
+      };
+    case SET_PAGE_COUNT:
+      return {
+        ...state,
+        pageCount: action.pageCount,
+      };
+    case SET_ACTIVE_PAGE:
+      return {
+        ...state,
+        activePage: action.activePage,
+      };
+    case SET_FETCH_STATE:
+      return {
+        ...state,
+        fetchState: action.fetchState,
+      };
     default:
       return state;
   }
