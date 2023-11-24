3b2eb3a01b55824652aa8d6c5b73fc68f158b49b
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-24 13:16:05 +0300

    filtered product according to description

--- src/companents/ProductListPage.js
+++ src/companents/ProductListPage.js
@@ -15,25 +15,9 @@ function ProductListPage() {
 
   const { products } = useSelector((s) => s.product.product);
 
-  console.log("State:", products);
   const [search, setSearch] = useState();
 
-  // const shopList = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
-
-  // const productList = [
-  //   { id: 1 },
-  //   { id: 2 },
-  //   { id: 3 },
-  //   { id: 4 },
-  //   { id: 5 },
-  //   { id: 6 },
-  //   { id: 7 },
-  //   { id: 8 },
-  //   { id: 9 },
-  //   { id: 10 },
-  //   { id: 11 },
-  //   { id: 12 },
-  // ];
+  console.log("Search:", search);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -82,12 +66,49 @@ function ProductListPage() {
           </div>
         </div>
         <button className="btn-blue md:mt-0">Filter</button>
+        <div className="mb-3 xl:w-96">
+          <div className="  mb-4 flex w-full flex-wrap items-stretch justify-center">
+            <input
+              type="search"
+              className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="button-addon2"
+            />
+
+            <span
+              className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+              id="basic-addon2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col md:flex-row flex-wrap gap-6 md:gap-20 justify-center items-center pt-10">
         {products ? (
-          products.map((product) => (
-            <ProductCardList key={product.id} product={product} />
-          ))
+          products
+            .filter((product) => {
+              const searchLowerCase = search ? search.toLowerCase() : "";
+              return searchLowerCase === ""
+                ? product
+                : product.description.toLowerCase().includes(searchLowerCase);
+            })
+            .map((product) => (
+              <ProductCardList key={product.id} product={product} />
+            ))
         ) : (
           <div role="status">
             <svg
